5384682d25b3d1ef8cb25bfd1d84e4438ec54b47
eileen <zwleileen@gmail.com>
2025-03-06 13:31:02 +0800

    debugged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,34 +13,22 @@ import ValuesResults from './components/ValuesResults/ValuesResults';
 
 function App() {
   const { user } = useContext(UserContext);
-  const [isNewUser, setIsNewUser] = useState(false);
-
-  useEffect(() => {
-    if(isNewUser) setIsNewUser(false);
-  }, [isNewUser]);
 
 return (
   <div className="min-h-screen flex flex-col bg-white">
   { user ? <NavBar /> : ''}
   <div className="flex-grow">
-  <Routes>
-    {/* Route for new user after sign up */}
-    <Route path="/values/new" element={user ? (<ValuesForm />) : <Navigate to="/" />} />
-    
+  <Routes>    
     {/* Routes available to all users */}
-    <Route path='/' element={ !user ? <Landing /> : ( isNewUser ? 
-      <Navigate to="/values/new" /> :
-      <Navigate to="/home" />
-    )} />
-    <Route path='/sign-up' element={!user ? <SignUpForm /> : <Navigate to="/home" />} />
+    <Route path='/' element={ !user ? <Landing /> : <Navigate to="/home" />} />
+    <Route path='/sign-up' element={!user ? <SignUpForm /> : <Navigate to="/values/new" />} />
     <Route path='/sign-in' element={!user ? <SignInForm /> : <Navigate to="/home" />} />
         
     {/* Protected routes - only available when logged in */}
     <Route path='/home' element={user ? <Homepage /> : <Navigate to="/" />} />
+    <Route path="/values/new" element={user ? (<ValuesForm />) : <Navigate to="/" />} />
     <Route path="/values/results" element={user ? (<ValuesResults />) : <Navigate to="/" />} />
         
-    {/* Fallback route for any other paths */}
-    <Route path="*" element={<Navigate to={user ? "/home" : "/"} />} />
   </Routes>
   </div>
   </div>
